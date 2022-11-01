13fe15307f0f2d97d6e4c5e4fb46dd18e1716db6
longhairy <londo112@hotmail.com>
2022-11-01 10:19:57 +0100

    updated IPrime

--- PrimeNumbersService/IPrime.cs
+++ PrimeNumbersService/IPrime.cs
@@ -9,5 +9,8 @@ namespace PrimeNumbersService
     public interface IPrime
     {
         public List<string> GetPrimeAndNonPrimeNumberCounter(int number, int number2);
+        public List<string> GetAllPrimeAndNonPrimeNumberbeforefirstNumber(int number, int number2);
+        public string ListOfPrimeInProcentBetween(int listofprimebetween);
+        public string ListOfPrimeInProcentbeforeLowest(int listofprimebefore);
     }
 }
