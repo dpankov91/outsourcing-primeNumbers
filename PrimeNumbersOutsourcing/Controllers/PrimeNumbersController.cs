576369c44ee28b6f07fd5d250a972645f8e86263
dpankov91 <dpankov91@gmail.com>
2022-11-01 15:57:20 +0700

    skeleton done

---
+++ PrimeNumbersOutsourcing/Controllers/PrimeNumbersController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PrimeNumbersOutsourcing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrimeNumbersController : ControllerBase
+    {
+        // GET: api/<PrimeNumbersController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PrimeNumbersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PrimeNumbersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PrimeNumbersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PrimeNumbersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
